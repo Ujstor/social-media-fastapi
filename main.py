5b36b8828a4013a52e086a5e1ba48fe5e2bf7e17
ujstor <astipan@gmail.com>
2023-05-14 19:58:28 +0200

    GET and POST req

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your post"}
+
+@app.post("/create-post")
+def create_post(payLoad: dict = Body(...)):
+    print(payLoad)
+    return {"new_post": f"title: {payLoad['title']} content: {payLoad['content']}"}
