1931ca6e9d505ba6d43a0ad8dfbf0ebe47119c0d
ujstor <astipan@gmail.com>
2023-05-21 14:11:29 +0200

    Only Retrieving Logged in User's post

--- app/routers/post.py
+++ app/routers/post.py
@@ -13,7 +13,8 @@ router = APIRouter(
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     #cursor.execute("""SELECT * FROM posts""")
     #posts = cursor.fetchall()
-    posts = db.query(models.Post).all()
+    posts = db.query(models.Post).filter(
+        models.Post.owner_id == current_user.id).all()
     return posts
 
 
@@ -36,6 +37,9 @@ def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post with id {id} not found")
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Not authorized to perform requested action")
     return post
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
