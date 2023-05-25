aac5b703aa9f854738ee899f74e328165efcbcc0
ujstor <astipan@gmail.com>
2023-05-25 23:30:58 +0200

    cors

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from . import models
 from .database import engine
 from .routers import post, users, auth, vote
 from .config import Settings
+from fastapi.middleware.cors import CORSMiddleware
 
 settings = Settings()
 
@@ -10,6 +11,16 @@ settings = Settings()
 #using alembic instead
 
 app = FastAPI()
+
+origins = ["https://www.google.com"] #or ["*"] for all origins
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(users.router)
