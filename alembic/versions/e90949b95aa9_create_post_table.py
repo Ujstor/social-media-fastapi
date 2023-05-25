afcecf0325a01550ec9a2e951cf105ad094bcef9
ujstor <astipan@gmail.com>
2023-05-25 21:51:21 +0200

    alembic revision

---
+++ alembic/versions/e90949b95aa9_create_post_table.py
@@ -0,0 +1,24 @@
+"""create post table
+
+Revision ID: e90949b95aa9
+Revises: 
+Create Date: 2023-05-25 21:50:28.914894
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e90949b95aa9'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
