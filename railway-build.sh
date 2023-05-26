c1dc3e8de62a9b9e9471cd32621ea70f944d9958
ujstor <astipan@gmail.com>
2023-05-26 20:45:56 +0200

    sh migration and start server

--- railway-build.sh
+++ railway-build.sh
@@ -2,3 +2,9 @@
 echo "Database migration started"
 alembic upgrade head
 echo "================================"
+echo "Database migration completed"
+echo "================================"
+echo "Starting server"
+uvicorn app.main:app --host=0.0.0.0 --port=${PORT:-5000}
+echo "================================"
+echo "Server started"
