be5fee3f8304feef1e1b0c045bb8e4b4454c1110
ujstor <astipan@gmail.com>
2023-05-28 13:29:05 +0200

    test classes

--- app/calculations.py
+++ app/calculations.py
@@ -9,3 +9,16 @@ def multiply(num1: int, num2: int):
 
 def divide(num1: int, num2: int):
     return num1 / num2
+
+class BankAccount:
+    def __init__(self, balance: int = 0):
+        self.balance = balance
+
+    def deposit(self, amount: int):
+        self.balance += amount
+
+    def withdraw(self, amount: int):
+        self.balance -= amount
+
+    def collect_interest(self):
+        self.balance *= 1.1
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,5 +1,5 @@
 import pytest
-from app.calculations import add, subtract, multiply, divide
+from app.calculations import add, subtract, multiply, divide, BankAccount
 
 
 @pytest.mark.parametrize("num1, num2, expected", [
@@ -19,3 +19,26 @@ def test_multiply():
 def test_divide():
     assert divide(6, 3) == 2
 
+
+def test_bank_set_initial_balance():
+    bank_account = BankAccount(50)
+    assert bank_account.balance == 50
+
+def test_bank_default_amount():
+    bank_account = BankAccount()
+    assert bank_account.balance == 0
+
+def test_withdraw():
+    bank_account = BankAccount(100)
+    bank_account.withdraw(50)
+    assert bank_account.balance == 50
+
+def test_deposit():
+    bank_account = BankAccount(100)
+    bank_account.deposit(50)
+    assert bank_account.balance == 150
+
+def test_collect_interest():
+    bank_account = BankAccount(100)
+    bank_account.collect_interest()
+    assert round(bank_account.balance, 6) == 110
