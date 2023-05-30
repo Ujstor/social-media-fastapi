a2a94f9204e32f2ad33e033470782778378febb2
ujstor <astipan@gmail.com>
2023-05-30 22:54:49 +0200

    pytest

--- railway-build.sh
+++ railway-build.sh
@@ -5,6 +5,10 @@ alembic upgrade head
 echo "================================"
 echo "Database migration completed"
 echo "================================"
+echo "Testing started"
+echo "================================"
+pytest -v -s
+echo "================================"
 echo "Starting server"
 uvicorn app.main:app --host=0.0.0.0 --port=${PORT:-5000}
 
